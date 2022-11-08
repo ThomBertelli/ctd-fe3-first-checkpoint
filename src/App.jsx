c5548a7639a530e9cc10bbcc3ef40cf297abcda3
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-11-07 22:44:42 -0300

    ADD form + color cards and form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,90 @@
-
+import { useState } from 'react'
+import { Card } from './Card'
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
+  const [colorName, setColorName] = useState('')
+  const [colorCode, setColorCode] = useState('')
+  const [colorsList, setColorsList] = useState([])
+  const [formError, setFormError] = useState(false)
 
+  const regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
+
+  function addColorCard(event){
+
+    event.preventDefault()
+
+    const newColor = {
+
+      colorName : colorName,
+      colorCode: colorCode
+
+    }
+
+
+
+    if(colorName.trim().length < 3 || colorCode.trim().length < 6 || !regex.test(colorCode)) {
+
+      setFormError(true)
+
+    }else{
+
+      setFormError(false)
+
+      setColorsList([...colorsList, newColor])
+
+      setColorName('')
+      setColorCode('')
+
+
+    }
+
+
+
+    
+
+  }
 
   return (
     <div className="App">
      <h1>Carga de estudiantes</h1>
-     <form>
+     <form className={formError ? 'form-error' : ''} onSubmit={event => addColorCard(event)}>
       {/* Comece a desenvolver o seu Código por aqui :) */}
+        
+        <h2>Adicionar nova cor</h2>
+        
+        <label htmlFor="colorName">Nome</label>
+        <input type="text" id="colorName" value={colorName} onChange={event => setColorName(event.target.value)} />
+
+        <label htmlFor="color">Cor</label>
+        <input type="text" id="color" value={colorCode} onChange={event => setColorCode(event.target.value)} />
+
+        <button type='submit'> Adicionar </button>
+        <span className='errorMessage'> Por favor, verifique os dados inseridos no formulário </span>
      </form>
-    </div>
+
+      <div>
+        <h2>Cores Favoritas</h2>
+          {
+            colorsList.map(
+              colorData => {
+                return(
+
+                  <Card colorData={colorData}/>
+
+                )
+
+              }
+
+            )
+
+          }
+
+      </div>
+
+    </div>    
+
   )
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
   return (
-    <div>
-
+    <div className="color-card" style={{backgroundColor: props.colorData.colorCode}} >
+      <h3>{props.colorData.colorName}</h3>
+      <h2>{props.colorData.colorCode}</h2>
     </div>
   )
 }
